af03b6a05263913919a2c356cc898f20918f2cdb
catalan <pascalou06@hotmail.com>
2018-09-22 18:13:09 +0200

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(){
+    // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDxiHdgSBwLl9ZZOMK_ThnbOlZ2iA4qCoQ",
+    authDomain: "projet-book-openclass.firebaseapp.com",
+    databaseURL: "https://projet-book-openclass.firebaseio.com",
+    projectId: "projet-book-openclass",
+    storageBucket: "projet-book-openclass.appspot.com",
+    messagingSenderId: "30128538343"
+  };
+  firebase.initializeApp(config);
+  }
 }
